1ffac2e7677f814fa3d1333bee0a2633405faaab
humblezzz <884855071@qq.com>
2023-08-15 21:25:09 +0800

    hot-fix first commit

--- src/main/java/com/humblezzz/Hello.java
+++ src/main/java/com/humblezzz/Hello.java
@@ -4,5 +4,6 @@ public class Hello {
     public static void main(String[] args) {
         System.out.println("Git1");
         System.out.println("Git2");
+        System.out.println("Git hot");
     }
 }
